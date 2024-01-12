894ca5069916288f8ab9fc6c41e58aa467083f28
salonikarma <101549960+salonikarma@users.noreply.github.com>
2024-01-12 20:27:01 +0530

    Add files via upload

---
+++ palidrome.java
@@ -0,0 +1,20 @@
+public class palidrome {
+    public static boolean isPalidrome(String str){
+        int n= str.length();
+        for(int i=0;i<n/2;i++){
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+
+
+        }
+        return true;
+
+    }
+    public static void main(String args[]){
+        String str ="racecar";
+        System.out.println(isPalidrome(str));
+    }
+
+}
+
